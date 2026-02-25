5e08b63021c86f0fec864e74d0b45478b841c92b
macalcinoarq-city <macalcino.arq@gmail.com>
2026-02-25 12:36:24 -0500

    Create reportes.js

---
+++ api/reportes.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+  const SUPABASE_URL = "https://esgprjbtkydkzhpkjie.supabase.co/rest/v1/rpc/reportes_geojson_fc";
+  const SUPABASE_KEY = process.env.SUPABASE_ANON_KEY;
+
+  const response = await fetch(SUPABASE_URL, {
+    method: "POST",
+    headers: {
+      "apikey": SUPABASE_KEY,
+      "Authorization": `Bearer ${SUPABASE_KEY}`,
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({})
+  });
+
+  const data = await response.json();
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.status(200).json(data);
+}
